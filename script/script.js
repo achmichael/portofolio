2ffd58b435d6ff6b54627d51ceff92cc499fec9e
achmichael <achmadmichael4@gmail.com>
2024-06-27 10:33:42 +0700

    Update

--- script/script.js
+++ script/script.js
@@ -18,16 +18,73 @@ document.addEventListener('click', function (e){
         container.classList.remove('active');
     }
 });
+function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 const send = document.querySelector('.btn');
-send.addEventListener ('click', function (e){
-    e.preventDefault();
-    if (send.contains(e.target)){
+send.addEventListener('click', function(e) {
+    const nama = document.querySelector('.nama');
+    const email = document.querySelector('.email');
+    const message = document.querySelector('.message');
+
+
+    // Pengecekan input kosong
+    if (nama.value.trim() === '') {
+        e.preventDefault();
         Swal.fire({
-            title: "Message Send Successfully",
-            text: "Thanks for sending this message😍😍😍",
+            title: "Name is required",
+            text: "Please enter your name",
+            icon: "error",
+            confirmButtonText: "Ok",
+            confirmButtonColor: "blue"
+        });
+    } else if (message.value.trim() === '') {
+        e.preventDefault();
+        Swal.fire({
+            title: "Message is required",
+            text: "Please enter your message",
+            icon: "error",
+            confirmButtonText: "Ok",
+            confirmButtonColor: "blue"
+        });
+    }else if (!isValidEmail(email.value.trim())) {
+        e.preventDefault();
+        Swal.fire({
+            title: "Invalid Email",
+            text: "Please enter a valid email address",
+            icon: "error",
+            confirmButtonText: "Ok",
+            confirmButtonColor: "blue"
+        });
+        return; // Menghentikan eksekusi lebih lanjut jika email tidak valid
+    } else {
+        e.preventDefault();
+        // Jika semua input valid, tampilkan pesan sukses
+        Swal.fire({
+            title: "Message Sent Successfully",
+            text: "Thanks for sending this message 😍",
             icon: "success",
             confirmButtonText: "Ok",
             confirmButtonColor: "blue"
         });
     }
 });
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+  const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (currentScroll > lastScrollTop) { // Scroll ke bawah
+    const imageElement = document.querySelector('.image');
+    if (imageElement) {
+      imageElement.classList.add('parallax'); // Tambahkan kelas parallax
+    }
+  } else { // Scroll ke atas
+    const imageElement = document.querySelector('.image');
+    if (imageElement) {
+      imageElement.classList.remove('parallax'); // Hapus kelas parallax
+    }
+  }
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Untuk menangani pengguliran ke atas di bagian atas halaman
+}, false);
